2ef5085e3412513a8ab2509d87bbffd5c821ada5
Sahil Vaddoriya <112296220+sahilpatel444@users.noreply.github.com>
2025-01-27 13:52:39 +0530

    analitics

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Login from "./Components/Login/login.js"
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Home from "./Components/Home.js";
 import Downloader from "./Components/VideoDownload/Downloader.js";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const clientId = "571743621526-3lpmovu7hm9i31o7chqsa60vt7ikd3a6.apps.googleusercontent.com";
@@ -19,6 +20,7 @@ function App() {
       <BrowserRouter>
         <InputProvider>
         <GoogleOAuthProvider clientId={clientId}>
+            <Analytics />
             <Header />
      
           <Routes>
